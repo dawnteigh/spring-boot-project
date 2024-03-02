b882b5ef72374017a262c8e6b6f9e9ae3c1448c2
Donte Ladatto <donte279@gmail.com>
2024-03-01 23:14:19 -0600

    get content

--- content-calendar/src/main/java/com/example/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/com/example/contentcalendar/controller/ContentController.java
@@ -1,10 +1,26 @@
 package com.example.contentcalendar.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.contentcalendar.model.Content;
+import com.example.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
+  private final ContentCollectionRepository repository;
+
+  public ContentController(ContentCollectionRepository contentCollectionRepository) {
+    this.repository = contentCollectionRepository;
+  }
+
+  @GetMapping("")
+  public List<Content> findAll() {
+    return repository.findAll();
+  }
 }
