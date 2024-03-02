4fa95f26f379a96e53eaf390d4ed629d0d09b544
Donte Ladatto <donte279@gmail.com>
2024-03-01 23:14:45 -0600

    create content to test GET

--- content-calendar/src/main/java/com/example/contentcalendar/repository/ContentCollectionRepository.java
+++ content-calendar/src/main/java/com/example/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,5 +1,6 @@
 package com.example.contentcalendar.repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,10 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 import com.example.contentcalendar.model.Content;
+import com.example.contentcalendar.model.Status;
+import com.example.contentcalendar.model.Type;
+
+import jakarta.annotation.PostConstruct;
 
 @Repository
 public class ContentCollectionRepository {
@@ -23,5 +28,20 @@ public class ContentCollectionRepository {
 
   public Optional<Content> findById(Integer id) {
     return content.stream().filter(c -> c.id().equals(id)).findFirst();
+  }
+
+  @PostConstruct
+  private void init() {
+    Content c = new Content(
+        1,
+        "My First Blog Post",
+        "My first post",
+        Status.IDEA,
+        Type.ARTICLE,
+        LocalDateTime.now(),
+        null,
+        "");
+
+    content.add(c);
   }
 }
