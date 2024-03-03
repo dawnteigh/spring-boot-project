520f4e55cfaa83d30d506216c0971d9626ac4eea
Donte Ladatto <donte279@gmail.com>
2024-03-03 10:40:12 -0600

    allow cors

--- content-calendar/src/main/java/com/example/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/com/example/contentcalendar/controller/ContentController.java
@@ -11,6 +11,7 @@ import com.example.contentcalendar.model.Content;
 import com.example.contentcalendar.repository.ContentCollectionRepository;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
 
   private final ContentCollectionRepository repository;
