8c88f69dbb6633842983a99b2186b891f35daec9
Donte Ladatto <donte279@gmail.com>
2024-03-09 21:58:38 -0600

    Create content repo interface

---
+++ content-calendar/src/main/java/com/example/contentcalendar/repository/ContentRepository.java
@@ -0,0 +1,10 @@
+package com.example.contentcalendar.repository;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import com.example.contentcalendar.model.Content;
+
+public interface ContentRepository extends ListCrudRepository<Content,Integer>{
+
+
+}
